546f0ea0d317f4795b9e8635a7b2fd2c43c39a40
Oroko <aoroko09@gmail.com>
2021-01-16 14:39:03 -0500

    Added get and post routes for sign up and fetched the user name with the specific id to display on users index page/ home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,46 @@
-class UsersController < ApplicationController
+# frozen_string_literal: true
 
+class UsersController < ApplicationController
   # GET: /users
-  get "/users" do
+  get '/users' do
+    @user = User.find(session[:user_id])
     erb :"/users/index.html"
   end
 
   # GET: /users/new
-  get "/users/signup" do
+  get '/signup' do
     erb :"/users/signup.html"
   end
 
   # POST: /users
-  post "/users" do
-    redirect "/users"
+  post '/signup' do
+    
+    @user = User.create(
+      username: params['username'],
+      email: params['email'],
+      password: params['password']
+    )
+    session[:user_id] = @user.id
+    redirect '/users'
   end
 
   # GET: /users/5
-  get "/users/:id" do
+  get '/users/:id' do
     erb :"/users/show.html"
   end
 
   # GET: /users/5/edit
-  get "/users/:id/edit" do
+  get '/users/:id/edit' do
     erb :"/users/edit.html"
   end
 
   # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
+  patch '/users/:id' do
+    redirect '/users/:id'
   end
 
   # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
+  delete '/users/:id/delete' do
+    redirect '/users'
   end
 end
