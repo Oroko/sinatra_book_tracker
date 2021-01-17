6b9b1c034c170385322f7e7f10866ad2ad6f5d7e
Oroko <aoroko09@gmail.com>
2021-01-17 13:31:52 -0500

    Added logging in and logging out functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,28 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :welcome
   end
+
+  get '/login' do
+    erb :'/users/login.html'
+  end
+
+  post '/login' do
+    @user = User.find_by(
+      email: params['email']
+    )
+
+    if @user&.authenticate(params[:password])
+      session[:user_id] = @user.id
+
+      redirect '/books'
+    else
+      redirect '/'
+
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
 end
