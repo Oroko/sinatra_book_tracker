c0fe432722e44d19baae797b86094601b4070737
Oroko <aoroko09@gmail.com>
2021-01-17 13:43:21 -0500

    Added helper methods for the logging in functionality and set the route on the login form to login

---
+++ app/helpers/helpers.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Helpers
+  def self.current_user(session_hash)
+    @user = User.find(session_hash[:user_id])
+    @user
+  end
+
+  def self.logged_in?(session_hash)
+    !!session_hash[:user_id]
+  end
+end
