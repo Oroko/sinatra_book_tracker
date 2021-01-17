8e6506f02be1c8d771613296548c146a78502236
Oroko <aoroko09@gmail.com>
2021-01-17 13:33:44 -0500

    No changes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,37 +1,39 @@
-class BooksController < ApplicationController
+# frozen_string_literal: true
 
+class BooksController < ApplicationController
   # GET: /books
-  get "/books" do
+  get '/books' do
+    
     erb :"/books/index.html"
   end
 
   # GET: /books/new
-  get "/books/new" do
+  get '/books/new' do
     erb :"/books/new.html"
   end
 
   # POST: /books
-  post "/books" do
-    redirect "/books"
+  post '/books' do
+    redirect '/books'
   end
 
   # GET: /books/5
-  get "/books/:id" do
+  get '/books/:id' do
     erb :"/books/show.html"
   end
 
   # GET: /books/5/edit
-  get "/books/:id/edit" do
+  get '/books/:id/edit' do
     erb :"/books/edit.html"
   end
 
   # PATCH: /books/5
-  patch "/books/:id" do
-    redirect "/books/:id"
+  patch '/books/:id' do
+    redirect '/books/:id'
   end
 
   # DELETE: /books/5/delete
-  delete "/books/:id/delete" do
-    redirect "/books"
+  delete '/books/:id/delete' do
+    redirect '/books'
   end
 end
