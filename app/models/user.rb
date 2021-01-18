7bc3558ffe90f9347800fbe5b446e33e8f749214
Oroko <aoroko09@gmail.com>
2021-01-18 12:17:12 -0500

    Added the macro for input validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base 
-  has_many :books , dependent: :destroy
+  has_many :books, dependent: :destroy
   has_secure_password
   validates_presence_of :username, :email, :password
 end
