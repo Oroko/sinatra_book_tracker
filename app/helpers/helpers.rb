6daf8ccec801c8798e987cdcc48f2b573a2a5f5d
Oroko <aoroko09@gmail.com>
2021-01-19 21:17:41 -0500

    Added my helper methods in the app controller

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-class Helpers
-  def self.current_user(session_hash)
-    @user = User.find(session_hash[:user_id])
-    @user
-  end
+# class Helpers
+#   def self.current_user(session_hash)
+#     @user ||= User.find(session_hash[:user_id]) if session[:user_id]
+#     @user
+#   end
 
-  def self.logged_in?(session_hash)
-    !!session_hash[:user_id]
-  end
-end
+#   def self.logged_in?(session_hash)
+#     !!session_hash[:user_id]
+#   end
+# end
