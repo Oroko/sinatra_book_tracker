00152cbb7c391d204a5ab3aa31d273596a587fb1
Oroko <aoroko09@gmail.com>
2021-01-21 17:21:14 -0500

    moved helper methods here

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,14 @@ class BooksController < ApplicationController
   # GET: /books
   get '/books' do
     @books = current_user.books
+
+    erb :"/books/index.html"
+  end 
+
+  get '/books/filter/:status' do
+    @books = current_user.books.where(status: params[:status]) 
     
+
     erb :"/books/index.html"
   end
 
@@ -16,28 +23,43 @@ class BooksController < ApplicationController
   # POST: /books
   post '/books' do
     @books = current_user.books.build(params)
+
     @books.save
-    
+
     redirect '/books'
   end
 
   # GET: /books/5
   get '/books/:id' do
+    @book = current_user.books.find(params[:id])
+    # @books = current_user.books
+    # binding.pry
     erb :"/books/show.html"
   end
 
   # GET: /books/5/edit
   get '/books/:id/edit' do
+    @book = current_user.books.find(params[:id])
     erb :"/books/edit.html"
   end
 
   # PATCH: /books/5
   patch '/books/:id' do
-    redirect '/books/:id'
+   
+    @book = current_user.books.find(params[:id])
+    @book.title = params[:title]
+    @book.author = params[:author]
+    @book.genre = params[:genre]
+    @book.status = params[:status]
+    @book.pages = params[:pages]
+    @book.save
+    redirect "/books/#{params[:id]}"
   end
 
   # DELETE: /books/5/delete
   delete '/books/:id/delete' do
+    @book = current_user.books.find(params[:id])
+    @book.delete
     redirect '/books'
   end
 end
