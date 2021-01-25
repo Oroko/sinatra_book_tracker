fe8b4042fe2e6823efc972039a12d8cb04a0fd23
Oroko <aoroko09@gmail.com>
2021-01-25 17:20:45 -0500

    Nothing changed

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,12 +1,21 @@
 # frozen_string_literal: true
 
-# class Helpers
-#   def self.current_user(session_hash)
-#     @user ||= User.find(session_hash[:user_id]) if session[:user_id]
-#     @user
-#   end
+# class Helpers < ApplicationController
+#   # def self.current_user(session_hash)
+#   #   @user ||= User.find(session_hash[:user_id]) if session[:user_id]
+#   #   @user
+#   # end
 
-#   def self.logged_in?(session_hash)
-#     !!session_hash[:user_id]
+#   # def self.logged_in?(session_hash)
+#   #   !!session_hash[:user_id]
+#   # end
+
+#   def self.checkLogin
+#     puts 'Check_______________________________________'
+#     puts sessions
+#     puts helpers
+#     # redirect '/login' unless log_in?
+#     puts 'End Check___________________________________'
+#     self
 #   end
 # end
