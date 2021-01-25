118bb8babac2890632c7cb33ef6f8e4472dbe2cb
Oroko <aoroko09@gmail.com>
2021-01-25 17:22:21 -0500

    Added the class method to filter through the status column in the books table

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,9 @@
 
 class Book < ActiveRecord::Base
   belongs_to :user
-  
+  validates_presence_of :title, :author, :genre, :status, :pages
+
+  def self.filter_status(params)
+    where(status: params[:status])
+  end
 end
